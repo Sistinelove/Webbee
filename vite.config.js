3c6b8d132bb8ac6be2abccde4f561ec405dc3bb3
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-05-23 03:42:43 +0500

    2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/',
+    base: '/Webbee/',
     build: {
         outDir: 'dist',
         rollupOptions: {
